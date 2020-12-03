4170dee6f0c1d594078c3377ac90b57447d2fce0
Terry Dover <72878519+TerryDover@users.noreply.github.com>
2020-12-02 23:41:31 -0600

    changed js and html for more conversions

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ function temperatureConverter(valNum) {
   valNum = parseFloat(valNum);
   document.getElementById("outputCelcius").innerHTML=(valNum-32)/1.8;
 }
+
+// Changed js to convert C to F
+function temperatureConverter1(valNum) {
+  valNum = parseFloat(valNum);
+  document.getElementById("outputFahrenheit").innerHTML=(valNum * 1.8) + 32;
+}
