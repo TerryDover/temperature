f5039f36fdcc5bef30e3c6db020a24a30bd53786
Terry Dover <72878519+TerryDover@users.noreply.github.com>
2020-12-02 22:47:45 -0600

    typed a bunch of stuff

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 // JS GOES HERE
+/* When the input field receives input, convert the value from fahrenheit to celsius */
+function temperatureConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputCelsius").innerHTML = (valNum-32) / 1.8;
+  }
+  fuction cToFtemperature(cVal);{
+      cVal = parseFloat (cVal);
+      document.getElementById("outputFahrenheit").innerHTML = (cVal * 1.8) +32;
+  }
+// °C to °F	Divide by 5, then multiply by 9, then add 32
+// °F to °C	Deduct 32, then multiply by 5, then divide by 9
