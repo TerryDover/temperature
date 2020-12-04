74dddf5bdffd17f13f66f821d9e3e88028cfb1e6
Terry Dover <72878519+TerryDover@users.noreply.github.com>
2020-12-03 20:52:00 -0600

    fixed up the padding and added color to the text

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 function temperatureConverter(valNum) {
-  valNum = parseFloat(valNum);
   document.getElementById("outputCelcius").innerHTML=(valNum-32)/1.8;
 }
 
 // Changed js to convert C to F
 function temperatureConverter1(valNum) {
-  valNum = parseFloat(valNum);
   document.getElementById("outputFahrenheit").innerHTML=(valNum * 1.8) + 32;
 }
